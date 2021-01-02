54517b1d3e19a6ab7f18338796c2c1a49c6da549
Ilya Zaytsev <ilya@zaytsev.xyz>
2021-01-02 19:17:01 +0300

    fourth

---
+++ fourth.hs
@@ -0,0 +1,54 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "Maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "Maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0    = []
+  | otherwise = x:replicate' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _ | n <= 0 = []
+take' _ []         = []
+take' n (x:xs)     = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+tr = take 5 (repeat 3)
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+xy = zip' [0, 1, 2, 3, 4] [3, 2, 1]
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x    = True
+  | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let less = quicksort [a | a <- xs, a <= x]
+      greater = quicksort [a | a <- xs, a > x]
+  in less ++ [x] ++ greater
+
+qs = quicksort [7, 10, 100, 77, -2, 256, 237, 0, 1, 2, 5, 5, 7]
+hw = quicksort "Hello, world!"
